7fcfcdd5c3959a36e5a0c11573040254559bb4c6
KAMIYA.Atsuteru <a-kamiya@pscsrv.co.jp>
2020-03-18 13:21:22 +0900

    プロジェクト ファイルを追加します。

---
+++ ReactiveUI871/MessageBusTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReactiveUI;
+
+namespace ReactiveUI871
+{
+    [TestClass]
+    public class MessageBusTest
+    {
+        [TestMethod]
+        public void MessageBusSequentialCallTest()
+        {
+            var syncer = new object();
+            var disoacher = Dispatcher.CurrentDispatcher;
+
+            Task.Run(() =>
+            {
+                Debug.WriteLine(string.Format("ManagerThreadID: {0}", Thread.CurrentThread.ManagedThreadId));
+                disoacher = Dispatcher.CurrentDispatcher;
+                Dispatcher.Run();
+            });
+
+            Task.Run(() =>
+            {
+                //var mainScheduler = CurrentThreadScheduler.Instance;
+                Debug.WriteLine(string.Format("MainThreadID: {0}", Thread.CurrentThread.ManagedThreadId));
+
+                var messageBus = new MessageBus();
+                //messageBus.RegisterScheduler<int>(CurrentThreadScheduler.Instance);
+                messageBus.Listen<int>().Subscribe(i =>
+                {
+                    disoacher.Invoke(() => 
+                    {
+                        Debug.WriteLine(string.Format("OUT:TaskNumber:{0}, ThreadId:{1}", i, Thread.CurrentThread.ManagedThreadId));
+                    });
+                });
+
+                Parallel.ForEach(new int[] { 1, 2, 3, 4, 5 }, i =>
+                {
+                    lock (syncer)
+                    {
+                        Debug.WriteLine(string.Format("IN:TaskNumber:{0}, ThreadId:{1}", i, Thread.CurrentThread.ManagedThreadId));
+                        Task.Run(() => messageBus.SendMessage(i));
+                    }
+                });
+
+            }).Wait();
+        }
+    }
+}
---
+++ ReactiveUI871/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReactiveUI871")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReactiveUI871")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("599998d0-f062-4ad3-9bf6-7f2429dbda8c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
